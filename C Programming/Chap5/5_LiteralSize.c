b3ffc8df7d4cf6d41f605882276b2c25fa1bdec7
Piru <yunroun@naver.com>
2024-04-03 22:13:57 +0900

    2024.04.03

---
+++ C Programming/Chap5/5_LiteralSize.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("literal int size: %d \n", sizeof(7));
+	printf("literal double size: %d \n", sizeof(7.14));
+	printf("literal char size: %d \n", sizeof('A'));
+	return 0;
+}
