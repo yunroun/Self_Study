306e2f2d5bfd0c5e1a3b8683d74f34655b5cbdea
Piru <yunroun@naver.com>
2024-05-09 21:34:26 +0900

    2024.05.09

--- C Programming/Chap19/4_ArgcArgv.c
+++ C Programming/Chap19/4_ArgcArgv.c
@@ -1,4 +1,4 @@
-// ArgcArgv.c의 배열 마지막 NULL의 삽입 확인
+// main 함수를 통한 인자의 전달
 
 #include <stdio.h>
 
@@ -6,9 +6,8 @@ int main(int argc, char* argv[]) {
 	int i = 0;
 	printf("전달된 문자열의 수: %d \n", argc);
 
-	while (argv[i] != NULL) {
+	for (i = 0; i < argc; i++) {
 		printf("%d번째 문자열: %s \n", i + 1, argv[i]);
-		i++;
 	}
 	return 0;
 }
